265e2487ca48c740f7fe31b44a45cd9866a0856a
GladysHuancaLu <gladys.luana123@gmail.com>
2019-01-28 11:14:30 -0500

    Login Google

--- src/main.js
+++ src/main.js
@@ -1,5 +1,30 @@
 // Este es el punto de entrada de tu aplicacion
 
-import { myFunction } from './lib/index.js';
+import { loginGoogle } from './lib/index.js';
 
-myFunction();
+//myFunction();
+/*
+const loginGoogle = () => {
+    var provider = new firebase.auth.GoogleAuthProvider();
+    provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      var token = result.credential.accessToken;
+      // The signed-in user info.
+      var user = result.user;
+      console.log(user);
+      // ...
+    }).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // The email of the user's account used.
+      var email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+      var credential = error.credential;
+      // ...
+    });
+  }
+  */
+  
+document.getElementById("btn-google").addEventListener("click", loginGoogle, false);
