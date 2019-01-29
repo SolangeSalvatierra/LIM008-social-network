d5fd1525ed5e32c71f1bfa4a8d94f2eefdcda88a
SolangeCorySalvatierraQuispe <solangraf@gmail.com>
2019-01-28 19:58:06 -0500

    Creacion de Cuenta con Firebase

--- src/main.js
+++ src/main.js
@@ -1,5 +1,30 @@
 // Este es el punto de entrada de tu aplicacion
 
-import { myFunction } from './lib/index.js';
+/*import { registrar } from './lib/index.js';
+import { acceder } from './lib/index.js';
+import { observador } from './lib/index.js';
+import { aparecer} from './lib/index.js';
+import { cerrar } from './lib/index.js';
+import { verificar } from './lib/index.js';*/
+import {x} from './lib/index.js';
 
-myFunction();
+const prueba = () =>{
+    x();
+    //registrar();
+}
+
+const prueba2 = () =>{
+    console.log('probando2')
+    //acceder();
+}
+/*observador();
+aparecer();
+cerrar();
+verificar();*/
+
+
+const btnRegistrar = document.getElementById('btn-registrar');
+const btnAcceder = document.getElementById('btn-acceder');
+
+btnRegistrar.addEventListener('click', prueba);
+btnAcceder.addEventListener('click', prueba2);
