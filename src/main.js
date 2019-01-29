9eb12d27c9b6f0bd6aafcc3dbda032132b4a541a
SolangeCorySalvatierraQuispe <solangraf@gmail.com>
2019-01-28 20:16:17 -0500

    Separando archivos de login con Import y Export

--- src/main.js
+++ src/main.js
@@ -1,30 +1,25 @@
 // Este es el punto de entrada de tu aplicacion
 
-/*import { registrar } from './lib/index.js';
+import { registrar } from './lib/index.js';
 import { acceder } from './lib/index.js';
 import { observador } from './lib/index.js';
 import { aparecer} from './lib/index.js';
 import { cerrar } from './lib/index.js';
-import { verificar } from './lib/index.js';*/
-import {x} from './lib/index.js';
+import { verificar } from './lib/index.js';
 
-const prueba = () =>{
-    x();
-    //registrar();
-}
-
-const prueba2 = () =>{
-    console.log('probando2')
-    //acceder();
-}
-/*observador();
-aparecer();
-cerrar();
-verificar();*/
 
 
 const btnRegistrar = document.getElementById('btn-registrar');
 const btnAcceder = document.getElementById('btn-acceder');
 
-btnRegistrar.addEventListener('click', prueba);
-btnAcceder.addEventListener('click', prueba2);
+btnRegistrar.addEventListener('click', () => {
+    registrar();
+});
+btnAcceder.addEventListener('click', () => {
+    acceder();
+    observador();
+    verificar();
+    aparecer();
+});
+
+cerrar();
