cd49999b55cf9a556781c7c1d67910d9e622c4fd
Tu-username <tucorreo@gmail.com>
2019-02-04 10:07:43 -0500

    test de firebase

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,8 +1,36 @@
 // importamos la funcion que vamos a testear
-import { myFunction } from "../src/lib/index";
+//import { myFunction } from "../src/lib/index";
 
-describe('myFunction', () => {
-  it('debería ser una función', () => {
-    expect(typeof myFunction).toBe('function');
+//describe('myFunction', () => {
+  //it('debería ser una función', () => {
+  //  expect(typeof myFunction).toBe('function');
+ // });
+//});
+
+// configurando firebase mock
+const firebasemock = require('firebase-mock');
+const mockauth = new firebasemock.MockFirebase();
+const mockfirestore = new firebasemock.MockFirestore();
+mockfirestore.autoFlush();
+mockauth.autoFlush();
+
+global.firebase = firebasemock.MockFirebaseSdk(
+  // use null if your code does not use RTDB
+  path => (path ? mockdatabase.child(path) : null),
+  () => mockauth,
+  () => mockfirestore
+);
+
+// iniciando tests
+
+import { registrar } from "../src/controller/controller-firebase.js";
+
+describe('correo electronico', () => {
+  it('Debería poder iniciar sesion', () => {
+    return registrar('front@end.la', '123456')
+      .then((user) => {
+        expect(user.email).toBe('front@end.la')
+      })
   });
-});
+})
+
