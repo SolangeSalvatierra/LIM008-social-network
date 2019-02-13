69301e4f2f88cc6ab5b83fd5e50f47c0c8ae0cac
AlexisPescoran <alexisfer.18@gmail.com>
2019-02-13 17:02:59 -0500

    validación de password y mensaje de error en creación de cuenta, validación de email, password y mensaje de error en login

--- src/view-controller.js
+++ src/view-controller.js
@@ -8,34 +8,69 @@ export const signInOnSubmitCreate = () => {
   const email = document.querySelector('#email').value;
   const password = document.querySelector('#password').value;
   let ok = 0;
-  for(let i=0 ; i<email.length ; i++){
-    if(email[i].charCodeAt() === 64){         
-      for(let j=i ; j<email.length ; j++){
-        if(email[j].charCodeAt() === 46){ 
-          ok=1;          
-        }       
-      }  
+  //validando que los campos de email y password no esten vacíos 
+  if(email !== '' && password !== ''){
+    for(let i=0 ; i<email.length ; i++){
+      if(email[i].charCodeAt() === 64){         
+        for(let j=i ; j<email.length ; j++){
+          if(email[j].charCodeAt() === 46){ 
+            ok=1;          
+          }       
+        }  
+      }
     }
-  }
-  if(ok===1){
-    //Probando lo de la validación de correo electrónico
-  registrar(email, password)
-  .then(() => {
-    verificar()          
-    changeHash('/login')          
-  })
-  .catch(() => {})    
+    if(ok===1){
+      if(password.length>=6 && password.length<=10){
+      //Probando lo de la verificación de correo electrónico
+      registrar(email, password)
+      .then(() => {
+        verificar()          
+        changeHash('/login')          
+      })
+      .catch(() => {})
+      }else {
+        alert('la contraseña debe contener un mínimo de 6 caracteres')
+      }         
+
+    }else{
+      alert('Debe ingresar un email valido');
+    } 
+
   }else{
-    alert('Debe ingresar un email valido');
-  }    
+    alert('debe completar los campos requeridos');
+  }     
 }
 
 export const signInOnSubmit = () => {
   const email = document.querySelector('#email').value;
   const password = document.querySelector('#password').value;
-  acceder(email, password)
-    .then(() => changeHash('/post'))
-    .catch(() => { })
+  let ok = 0;
+  if(email !== '' && password !== ''){
+    for(let i=0 ; i<email.length ; i++){
+      if(email[i].charCodeAt() === 64){         
+        for(let j=i ; j<email.length ; j++){
+          if(email[j].charCodeAt() === 46){ 
+            ok=1;          
+          }       
+        }  
+      }
+    }
+    if(ok===1){    
+      if(password.length>=6 && password.length<=10){
+        acceder(email, password)
+        .then(() => changeHash('/post'))
+        .catch(() => { })
+      }else{
+        alert('su contraseña es incorrecta, debe contener un mínimo de 6 caracteres')
+      }  
+      
+    }else{
+      alert('Debe ingresar un email valido');
+    }
+  }else {
+    alert('debe completar los campos requeridos');
+  }
+ 
 }
 
 export const signInOnSubmitGoogle = () => {
