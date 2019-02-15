9c879d1f38563c27d8b2bd48d05cf31f349a329d
SolangeCorySalvatierraQuispe <solangraf@gmail.com>
2019-02-15 03:08:42 -0500

    validando el html de los templates

--- src/templates/createAccount.js
+++ src/templates/createAccount.js
@@ -1,33 +1,36 @@
 import {signInOnSubmitCreate, signInOnSubmitGoogle, signInOnSubmitFacebook, changeHash} from "../view-controller.js";
-//no es necesario poner el .js
-// import {formIngresarCuenta} from "./login.js"
 
 export  default () => {
     const form = document.createElement('form');
     const  contenidoForm  =  `
 
-    <div   class="conteiner2">
-    <img src="ima/traveller.1.png" class="img-logo">
-        <h2> Registro de usuarios </h2>
-            <div>
-                <label> con:</label>
-                <div>
-                <img id="auth-fb" alt="ico-fb" class="ico-fb" src="https://i.ibb.co/NmxyWjL/ico-fb.png" />
-                <img id="auth-google" alt="ico-google" class="ico-google" src="https://i.ibb.co/xgLXQrr/ico-gg.png" />
-                </div>
-                </div>
-                <label> ó </label>
-            <div>                
-            <input  class="text-field" id = "email" type = "email" placeholder = "Correo">
-            <input  class="text-field" id = "password" type = "password" placeholder = "Contraseña">
-            <button type = "button" id = "btn-crear"> Registrate </button>
-            </div>            
-           
-            <div>            
-                <label>¿Ya tienes una cuenta?<label>
-                <button type = "button" id = "btn-back-login">Inicia sesión</button>
-            </div>
+    <div class="conteiner2">
+    <img alt='logo-traveller' src="ima/traveller.1.png" class="img-logo">
+    <h2> Registro de usuarios </h2>
+
+    <div>
+        <label> con:</label>
+
+        <div>
+            <img id="auth-fb" alt="ico-fb" class="ico-fb" src="https://i.ibb.co/NmxyWjL/ico-fb.png" />
+            <img id="auth-google" alt="ico-google" class="ico-google" src="https://i.ibb.co/xgLXQrr/ico-gg.png" />
+        </div>
+
     </div>
+
+    <label> ó </label>
+
+    <div>
+        <input class="text-field" id="email" type="email" placeholder="Correo">
+        <input class="text-field" id="password" type="password" placeholder="Contraseña">
+        <button type="button" id="btn-crear"> Registrate </button>
+    </div>
+
+    <div>
+        <label>¿Ya tienes una cuenta?</label>
+        <button type="button" id="btn-back-login">Inicia sesión</button>
+    </div>
+</div>
     `;
     // const form = document.createElement('form');
     // form.setAttribute('id', 'login');
